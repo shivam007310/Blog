c0a2e8ec1af2d2d94dda138dad3069ac81fbfc37
Shivam Rana <shivamrana704.sr@gmail.com>
2024-01-11 22:42:00 +0530

    auth service

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import conf from "./conf/conf";
+
+function App() {
+  console.log(conf.appWriteUrl);
+  return (
+    <>
+      <h1>Hi this app is working</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/appwrite/auth.js
@@ -0,0 +1,18 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appWriteUrl)
+      .setProject(conf.appWriteProjectId);
+    this.account = new Account(this.client);
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
